7eb78f2a90991f2a76da476820a7c6ef0572f686
CarloAlbino <carloalbino@gmail.com>
2015-05-11 18:02:23 -0400

    Initial project commit.  Includes temp character with animations.  Also includes prototype sprite swapping scripts.

---
+++ SaveThePrincess/Assets/Scripts/CharacterCreation/TestingScripts/DisplayInfo.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class DisplayInfo : MonoBehaviour {
+	public Text[] textArray;
+	public CreateCombination[] characters;
+
+	void Update(){
+		for(int i = 0; i < textArray.Length; i++){
+			textArray[i].text = characters[i].GetCurrentComboString();
+		}
+	}
+}

a5048061539a336dca703d8835c06b5817bf9ef9
Jake Deacon <deaconj02@gmail.com>
2015-06-15 19:01:36 -0400

    0.33.1 : Animation Timing tweaks
    Sequencing Death animation properly

---
+++ Assets/My Assets/_Scripts/ItemCreation/ItemClasses/Potion.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Potion class.
+/// </summary>
+public class Potion : Item {
+	/// <summary>
+	/// The name of the class. Potion.
+	/// </summary>
+	public string className = "Potion";
+	/// <summary>
+	/// The animation parameter for the Potion class.
+	/// </summary>
+	public string animationParameter;
+
+}

21e428fbe2297f3eab2e9612de2941c3f6d85df3
Jake Deacon <deaconj02@gmail.com>
2015-06-16 14:26:36 -0400

    Added Dummy Background and confirm/cancel buttons
    Added background for placement and confirm/cancel buttons.

--- Assets/My Assets/_Scripts/Game/Scene Controllers/MerchantController.cs
+++ Assets/My Assets/_Scripts/Game/Scene Controllers/MerchantController.cs
@@ -8,22 +8,33 @@ public class MerchantController : MonoBehaviour {
     // player reference
     private PlayerController player;
 
-    // items for sale - need Key/value pairs here!
+    // items for sale 
     public Dictionary<string,int> items;
 
     //number of items for sale
     public int numItemsForSale;
 
+    private int purchaseBalance, 
+                playerBalance, 
+                remainingBalance;
+
     // text to display player balance
-    public Text playerBalance;
+    public Text playerBalanceText;
 
     // text to show total amount being purchased
-    public Text cartBalance;
+    public Text purchaseBalanceText;
+
+    // text to show remaining balance after purchase
+    public Text remainingBalanceText;
 
     // buttons for increase/decrease quantity
 
     // text to display amount of each item
 
+    // purchase button
+    public Button purchaseButton;
+    // cancel button
+    public Button cancelButton;
     // exit button
     public Button exitButton;
 
@@ -43,13 +54,30 @@ public class MerchantController : MonoBehaviour {
         this.items.Add("Large Energy Potion", 0);
         this.items.Add("Small Energy Potion", 0);
 	}
+    /// <summary>
+    /// Calculate Shop balances here to be updated
+    /// </summary>
+    void CalculateShop()
+    {
+        // get palyer balance
+        this.playerBalance = this.player.dollarBalance;
+        
+        // get item balance
+        //this.purchaseBalance = 
+
+        // calculate player's remaining balance
+        this.remainingBalance = this.playerBalance - this.purchaseBalance;
+    }
 
     void UpdateText()
     {
-        this.playerBalance.text = this.player.dollarBalance.ToString();
+        this.playerBalanceText.text = this.playerBalance.ToString();
+        this.remainingBalanceText.text = this.remainingBalance.ToString();
+        this.purchaseBalanceText.text = this.purchaseBalance.ToString();
     }
 
 	void Update () {
+        CalculateShop();
         UpdateText();
 	}
 }

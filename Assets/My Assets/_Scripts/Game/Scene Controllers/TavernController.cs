9ffcae7cffecf55e98cc9457053a6ee99203e4c3
Jake Deacon <deaconj02@gmail.com>
2015-06-17 11:25:58 -0400

    Script modifications to handle proper behaviour on clicks
    Needs Background
    Needs Sprites for each Meal option

--- Assets/My Assets/_Scripts/Game/Scene Controllers/TavernController.cs
+++ Assets/My Assets/_Scripts/Game/Scene Controllers/TavernController.cs
@@ -2,74 +2,122 @@ using UnityEngine;
 using UnityEngine.UI;
 using System.Collections;
 
-public class TavernController : MonoBehaviour {
+public class TavernController : MonoBehaviour
+{
 
-	public PlayerController player;
-	public int[] prices, stats;
-
-	public Button[] foodButtons;
+    public PlayerController player;
+    Vector3 prevPos;
+    // UI
     public Button sleepForNight;
-	public Text[] buttonText;
-	public Text healthText, energyText, playerBalance;
-	int BASE_MEAL_COST;
-	Vector3 prevPos;
+    public Text sleepText;
+    public Button[] foodButtons;
+    public Text[] buttonText;
+    // stats
+    public int[] prices, stats;
+    public Text healthText, energyText, playerBalance;
+    // Variables
+    int BASE_MEAL_COST, NUM_MEALS;
+    int totalStatsMissing;
 
-	// Use this for initialization
-	void Start () {
-		this.player = FindObjectOfType<PlayerController>();
-		this.prevPos = this.player.gameObject.transform.localPosition;
-		
-		// relocate player
-		Vector3 newSpot = new Vector3 (-5.5f, -3.7f);
-		this.player.gameObject.transform.localPosition = newSpot;
-        
-		BASE_MEAL_COST = 15;
-		this.healthText.text = "";
-		this.energyText.text = "";
-		this.playerBalance.text = "";
-		prices = new int[buttonText.Length];
-		stats = new int[buttonText.Length];
-        
-        // RANDOMIZE PRICING HERE
-		for (int i=0; i<buttonText.Length; i++) {
-			prices [i] = BASE_MEAL_COST + (i * 5);
-			buttonText [i].text = "SLEEP FOR NIGHT: $" + prices [i] + "";
-		}
-	}
+    // Use this for initialization
+    void Start()
+    {
+        BASE_MEAL_COST = 15;
+        NUM_MEALS = 3;
+        // get player
+        this.player = FindObjectOfType<PlayerController>();
+        // relocate player
+        this.prevPos = this.player.gameObject.transform.localPosition;
+        Vector3 newSpot = new Vector3(-5.5f, -3.7f);
+        this.player.gameObject.transform.localPosition = newSpot;
+        //init texts
+        this.healthText.text = "";
+        this.energyText.text = "";
+        this.playerBalance.text = "";
+        this.sleepText.text = "";
 
-	private void UpdateText(){
-		this.healthText.text = "Health: " + this.player.remainingHealth + "/" + this.player.totalHealth;
-		this.energyText.text = "Mana: " + this.player.remainingEnergy + "/" + this.player.totalEnergy;
-		this.playerBalance.text = "Balance: $" + this.player.dollarBalance;
-		for (int i=0; i<buttonText.Length; i++) {
-			buttonText[i].text = "SLEEP FOR NIGHT: $" + prices[i] + " ";
-		}
-	}
+        prices = new int[NUM_MEALS];
+        stats = new int[NUM_MEALS];
 
-	public void SleepForNight(int index){
-		// if player needs health OR mana
-		if (player.remainingEnergy < player.totalEnergy || player.remainingHealth < player.totalHealth) {
-			// if can afford, purchase
-			if (player.PurchaseItem (prices [index])) {
-				// effects administered here
-				// USING SIMPLE MATH HERE FOR NOW, DOLLAR PER 2 POINT OF MANA/HEALTH
-				player.GiveEnergy (prices [index]*2);
-				player.HealForAmount (prices [index]*2);
-			}
-		} else {
-			Debug.Log ("You already have full stats! Go fight something!");
-		}
-	}
+        //get total health missing on entry
+        totalStatsMissing = (this.player.totalHealth - this.player.remainingHealth) + (this.player.totalEnergy - this.player.remainingEnergy);
+
+        // RANDOMIZE MEAL PRICING HERE
+        for (int i = 0; i < buttonText.Length; i++)
+        {
+            prices[i] = BASE_MEAL_COST + (i * 5);
+            stats[i] = BASE_MEAL_COST + (i * 5);
+        }
+
+    }
+
+    private void UpdateText()
+    {
+        this.healthText.text = "Health: " + this.player.remainingHealth + "/" + this.player.totalHealth;
+        this.energyText.text = "Energy: " + this.player.remainingEnergy + "/" + this.player.totalEnergy;
+        this.playerBalance.text = "Balance: $" + this.player.dollarBalance;
+
+        for (int i = 0; i < buttonText.Length; i++)
+        {
+            buttonText[i].text = "Meal: $" + prices[i] + " ";
+        }
+
+        if (totalStatsMissing > 0)
+        {
+            this.sleepText.text = "Sleep: $" + totalStatsMissing;
+        }
+        else
+        {
+            this.sleepText.text = "";
+            this.sleepForNight.gameObject.SetActive(false);
+
+        }
+    }
+
+    public void PurchaseMeal(int index)
+    {
+        if (totalStatsMissing > 0)
+        {
+            if (player.PurchaseItem(prices[index]))
+            {
+                this.player.GiveEnergy(stats[index]);
+                this.player.HealForAmount(stats[index]);
+            }
+        }
+        else
+        {
+            Debug.Log("You already have full stats! Go fight something!");
+        }
+    }
+
+    public void SleepForNight()
+    {
+        // if player needs health OR mana
+        if (totalStatsMissing > 0)
+        {
+            // if can afford, purchase
+            if (player.PurchaseItem(totalStatsMissing))
+            {
+                Debug.Log(this.player.name + " is refreshed after a night of sleep!");
+            }
+        }
+        else
+        {
+            Debug.Log("You already have full stats! Go fight something!");
+        }
+    }
 
 
-	public void LeaveInn(){
-		this.player.gameObject.transform.localPosition = prevPos;
-		DontDestroyOnLoad (this.player);
-		Application.LoadLevel ("Town_LVP");
-	}
+    public void LeaveInn()
+    {
+        this.player.gameObject.transform.localPosition = prevPos;
+        DontDestroyOnLoad(this.player);
+        Application.LoadLevel("Town_LVP");
+    }
 
-	// Update is called once per frame
-	void Update () {
-		UpdateText ();
-	}
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateText();
+    }
 }

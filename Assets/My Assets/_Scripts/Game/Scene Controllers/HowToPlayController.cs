c2f5230323961599ac8ffdb3cc47a96e624a8585
CarloAlbino <carloalbino@gmail.com>
2015-06-23 16:23:57 -0400

    SecondStepMerge

--- Assets/My Assets/_Scripts/Game/Scene Controllers/HowToPlayController.cs
+++ Assets/My Assets/_Scripts/Game/Scene Controllers/HowToPlayController.cs
@@ -7,7 +7,7 @@ public class HowToPlayController : MonoBehaviour {
     public Button backButton;
 	// Use this for initialization
 	void Start () {
-	
+        EscapeHandler.instance.GetButtons();
 	}
 	
 	// Update is called once per frame
@@ -17,13 +17,21 @@ public class HowToPlayController : MonoBehaviour {
 
     public void GoBack()
     {
+<<<<<<< HEAD
         AudioManager.Instance.PlaySFX("Select");
+=======
+        EscapeHandler.instance.ClearButtons();
+>>>>>>> origin/Develop
         Application.LoadLevel("StartMenu_LVP");
     }
 
     public void StartGame()
     {
+<<<<<<< HEAD
         AudioManager.Instance.PlaySFX("Select");
+=======
+        EscapeHandler.instance.ClearButtons();
+>>>>>>> origin/Develop
         Application.LoadLevel("CharacterSelect_LVP");
     }
 }

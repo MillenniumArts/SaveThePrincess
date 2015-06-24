b7c33af4cd385d90b659db13df1c08e44e8256ed
Jake Deacon <deaconj02@gmail.com>
2015-06-24 13:38:12 -0400

    Added Panel for Notifications
    Title, Content etc

---
+++ Assets/My Assets/_Scripts/Game/NotificationHandler.cs
@@ -0,0 +1,72 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class NotificationHandler : MonoBehaviour {
+
+    public GameObject notificationPanel;
+    public Button confirm;
+    public Button[] buttons;
+    public Text title, content;
+
+    #region Singleton
+    private static NotificationHandler _instance;
+
+    public static NotificationHandler instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = GameObject.FindObjectOfType<NotificationHandler>();
+                DontDestroyOnLoad(_instance.gameObject);
+            }
+            return _instance;
+        }
+    }
+
+    void Awake()
+    {
+        if (_instance == null)
+        {
+            _instance = this;
+            DontDestroyOnLoad(this);
+        }
+        else
+        {
+            if (this != _instance)
+            {
+                Destroy(this.gameObject);
+            }
+        }
+    }
+    #endregion Singleton
+
+    public void MakeNotification(string nTitle, string nContent)
+    {
+        // set up content/title
+        this.title.text = nTitle;
+        this.content.text = nContent;
+        // enable panel
+        this.gameObject.SetActive(true);
+    }
+
+    public void Confirm()
+    {
+        // clear content
+        this.title.text = null;
+        this.content.text = null;
+        // disable panel
+        this.gameObject.SetActive(false);
+    }
+
+	// Use this for initialization
+	void Start () {
+        this.gameObject.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}

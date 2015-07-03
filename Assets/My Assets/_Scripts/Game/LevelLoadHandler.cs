8c851180639c1d7a7c1e9aeef6b27f7127faacff
Jake Deacon <deaconj02@gmail.com>
2015-07-03 17:05:04 -0400

    Added Level Load Handler Class

---
+++ Assets/My Assets/_Scripts/Game/LevelLoadHandler.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelLoadHandler : MonoBehaviour
+{
+    /// <summary>
+    /// This class will handle level loading throughout the game
+    /// </summary>
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public string sceneToLoad;
+
+    public void LoadLevel(string level)
+    {
+        Application.LoadLevel(level);
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}

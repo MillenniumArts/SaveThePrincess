0cf6d60397d2e8c7d9fb29e55d3857abf1590eb1
Jake Deacon <deaconj02@gmail.com>
2015-07-20 12:58:22 -0400

    Typo fix

--- Assets/My Assets/_Scripts/Audio/AnimationAudio.cs
+++ Assets/My Assets/_Scripts/Audio/AnimationAudio.cs
@@ -1,8 +1,8 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
 public class AnimationAudio : MonoBehaviour {
-    public void PlaySondEffect(string name)
+    public void PlaySoundEffect(string name)
     {
         AudioManager.Instance.PlaySFX(name);
     }

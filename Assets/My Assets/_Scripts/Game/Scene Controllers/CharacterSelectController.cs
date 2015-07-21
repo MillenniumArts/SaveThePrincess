9b23871a3519ff2875b8be9b90a304f73bb2fe01
Jake Deacon <deaconj02@gmail.com>
2015-07-21 12:11:29 -0400

    Adding Character Select Scene
    Scene Created
    Buttons Added for skin swapping
    Added Confirm Button
    Framed out methods for next/prev skins

---
+++ Assets/My Assets/_Scripts/Game/Scene Controllers/CharacterSelectController.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+using System.Collections;
+
+public class CharacterSelectController : MonoBehaviour {
+
+    public PlayerController player;
+
+    void Awake()
+    {
+        this.player = FindObjectOfType<PlayerController>();
+    }
+
+    /// <summary>
+    ///  Next Skin Button Call
+    /// </summary>
+    public void NextSkin()
+    {
+
+    }
+
+    /// <summary>
+    /// Previous skin button call
+    /// </summary>
+    public void PrevSkin()
+    {
+
+    }
+
+    public void Confirm()
+    {
+        // do something here before next load
+
+        LevelLoadHandler.Instance.LoadLevel("StatSelect_LVP");
+    }
+    
+    // Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}

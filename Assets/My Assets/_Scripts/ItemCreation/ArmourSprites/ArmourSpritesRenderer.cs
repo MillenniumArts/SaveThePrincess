9e27a108e698d28d4d6483e936a33d17472e4903
CarloAlbino <carloalbino@gmail.com>
2015-08-05 09:49:32 -0400

    Setup for lighter armour system

---
+++ Assets/My Assets/_Scripts/ItemCreation/ArmourSprites/ArmourSpritesRenderer.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ArmourSpritesRenderer : MonoBehaviour {
+    private NewArmourSprites armourSprites;
+    [SerializeField]
+    private SpriteRenderer armourRenderer, helmetRenderer;
+    [SerializeField]
+    private Sprite _ArmourSprite, _HelmetSprite;
+
+    void Start()
+    {
+        armourSprites = this.gameObject.GetComponent<NewArmourSprites>();
+        armourSprites.GenerateNewSprites();
+        SetSprites();
+    }
+
+    public void TransferSprites(NewArmourSprites other)
+    {
+        armourSprites.SetSpriteSheetName(other.GetSpriteSheetName());
+        armourSprites.SetArmourSprite(other.GetArmourSprite());
+        armourSprites.SetHelmetSprite(other.GetHelmetSprite());
+        SetSprites();
+    }
+
+    private void SetSprites()
+    {
+        _ArmourSprite = armourSprites.GetArmourSprite();
+        _HelmetSprite = armourSprites.GetHelmetSprite();
+        RenderSprites();
+    }
+
+    private void RenderSprites()
+    {
+        armourRenderer.sprite = _ArmourSprite;
+        helmetRenderer.sprite = _HelmetSprite;
+    }
+}
---
+++ Assets/My Assets/_Scripts/ItemCreation/ArmourSprites/NewArmourSprites.cs
@@ -0,0 +1,88 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
+
+public class NewArmourSprites : MonoBehaviour {
+    [SerializeField]
+    private string folder, spriteSheetName;
+    [SerializeField]
+    private Sprite chosenArmour, chosenHelmet;
+    private List<Sprite> _Armour, _Helmet;
+
+    void Start()
+    {
+        _Armour = new List<Sprite>();
+        _Helmet = new List<Sprite>();
+    }
+
+    public void GenerateNewSprites()
+    {
+        LoadSprites();
+        int rand1 = RandomNumberGenerator(_Armour.Count);
+        int rand2 = RandomNumberGenerator(_Helmet.Count);
+        SetChosenSprites(rand1, rand2);
+        _Armour.Clear();
+        _Helmet.Clear();
+        _Armour = new List<Sprite>();
+        _Helmet = new List<Sprite>();
+    }
+
+    public string GetSpriteSheetName()
+    {
+        return spriteSheetName;
+    }
+
+    public Sprite GetArmourSprite()
+    {
+        return chosenArmour;
+    }
+
+    public Sprite GetHelmetSprite()
+    {
+        return chosenHelmet;
+    }
+
+    public void SetSpriteSheetName(string _sheetName)
+    {
+        spriteSheetName = _sheetName;
+    }
+
+    public void SetArmourSprite(Sprite armour)
+    {
+        chosenArmour = armour;
+    }
+
+    public void SetHelmetSprite(Sprite helmet)
+    {
+        chosenHelmet = helmet;
+    }
+
+    private void LoadSprites()
+    {
+        Sprite[] _sprites = Resources.LoadAll<Sprite>("_Final_Assets/" + folder + "/" + spriteSheetName);
+        for (int i = 0; i < _sprites.Length; i++)
+        {
+            string[] tempNames = _sprites[i].name.Split('_');
+            if (tempNames[0] == "Helmet")
+            {
+                _Helmet.Add(_sprites[i]);
+            }
+            else
+            {
+                _Armour.Add(_sprites[i]);
+            }
+        }
+    }
+
+    private int RandomNumberGenerator(int MAX)
+    {
+        int r = Random.Range(0, MAX);
+        return r;
+    }
+
+    private void SetChosenSprites(int armourIndex, int helmetIndex)
+    {
+        chosenArmour = _Armour[armourIndex];
+        chosenHelmet = _Helmet[helmetIndex];
+    }
+}

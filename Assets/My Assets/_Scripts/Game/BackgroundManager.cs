87c50c053c0d87776b28e3142a2b35c5da6cc249
CarloAlbino <carloalbino@gmail.com>
2015-08-13 13:19:13 -0400

    Added new Background and Updated background manager

--- Assets/My Assets/_Scripts/Game/BackgroundManager.cs
+++ Assets/My Assets/_Scripts/Game/BackgroundManager.cs
@@ -5,20 +5,23 @@ using System.Collections;
 public class BackgroundManager : MonoBehaviour {
 
     public int numBackgrounds;
+    public string folder, spriteSheetName;
     public int backgroundNum = 0;
-    public Sprite[] backgroundImages;
+    //public Sprite[] backgroundImages;
     public Image background;
 
     public void SetBackground()
     {
+        Debug.Log("SetBackground Called");
         int newNum = PlayerPrefs.GetInt("BackgroundImageNum");
-        if(newNum < backgroundImages.Length)
+        if(newNum < numBackgrounds)
             backgroundNum = newNum;
-        background.sprite = backgroundImages[backgroundNum];
+        background.sprite = Resources.Load<Sprite>("_Final_Assets/Environments/" + folder + "/" + spriteSheetName + backgroundNum);//backgroundImages[backgroundNum];
     }
 
     public void BackgroundChange()
     {
+        Debug.Log("BackgroundChange Called");
         //Debug.Log("Yes BackgroundChange gets called.");
         backgroundNum = Random.Range(0, numBackgrounds);
         PlayerPrefs.SetInt("BackgroundImageNum", backgroundNum);

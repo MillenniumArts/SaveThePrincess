05b7d8255559148ec7daa3eff8d0eb5f3e3f0475
CarloAlbino <carloalbino@gmail.com>
2015-09-02 11:39:15 -0400

    Basic Ads ready to go.
    Ads are initialized for iOS and Android.
    There is a button in the town scene to play an ad and get rewarded.
    Unity Ads Package imported.

---
+++ Assets/My Assets/_Scripts/Ads/AdManager.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Advertisements;
+
+public class AdManager : MonoBehaviour {
+    [SerializeField]
+    private int reward = 100;
+
+    public void ShowAd(string zoneID = "")
+    {
+        #if UNITY_EDITOR
+            StartCoroutine(WaitForAd());
+        #endif
+
+        if (zoneID == "")
+        {
+            zoneID = null;
+        }
+
+        ShowOptions options = new ShowOptions();
+        options.resultCallback = AdCallBackHandler;
+
+        if (Advertisement.IsReady(zoneID))
+        {
+            Advertisement.Show(zoneID, options);
+        }
+        else if (Advertisement.IsReady())
+        {
+            Advertisement.Show(null, options);
+        }
+        else
+        {
+            Debug.Log("No ads are ready.");
+        }
+    }
+
+
+    private void AdCallBackHandler(ShowResult result)
+    {
+        switch (result)
+        {
+            case ShowResult.Finished:
+                FindObjectOfType<PlayerController>().dollarBalance += reward;
+                break;
+            case ShowResult.Skipped:
+                FindObjectOfType<PlayerController>().dollarBalance += (reward/4);
+                break;
+            case ShowResult.Failed:
+                Debug.Log("The Show Result of the Ad is 'Failed'");
+                break;
+        }
+    }
+
+    private IEnumerator WaitForAd()
+    {
+        float currentTimeScale = Time.timeScale;
+        Time.timeScale = 0f;
+        yield return null;
+
+        while (Advertisement.isShowing)
+        {
+            yield return null;
+        }
+
+        Time.timeScale = currentTimeScale;
+    }
+}
---
+++ Assets/My Assets/_Scripts/Ads/AdViewer.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Advertisements;
+
+public class AdViewer : MonoBehaviour {
+    [SerializeField]
+    private int reward = 50;
+    [SerializeField]
+    private float cooldownTime = 3.0f;
+    private bool canWatch = true;
+
+    private PlayerController player;
+
+    void Start()
+    {
+        player = FindObjectOfType<PlayerController>();
+    }
+
+    public void AskForAds()
+    {
+        if (canWatch)
+        {
+            // Open dialog for asking to show an ad.
+        }
+        else
+        {
+            // Show notification to wait before watching an ad.
+        }
+    }
+
+    public void StartAd()
+    {
+        if (Advertisement.IsReady())
+        {
+            Advertisement.Show();
+        }
+    }
+
+    public void CancelAd()
+    {
+        // Close ad dialog.
+    }
+
+    private void CheckIfWatched()
+    {
+        
+        StartCoroutine("RewardPlayer");
+    }
+
+    private IEnumerator RewardPlayer()
+    {
+        canWatch = false;
+        player.dollarBalance += reward;
+        // Show notification that player has got a reward.
+        yield return new WaitForSeconds(cooldownTime);
+        canWatch = true;
+    }
+
+}

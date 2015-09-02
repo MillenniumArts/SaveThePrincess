21da6f0d5ce3e8c818bb07295c377d6f55b51970
CarloAlbino <carloalbino@gmail.com>
2015-09-02 12:33:50 -0400

    Ui for viewing an ad now implemented.

---
+++ Assets/My Assets/_Scripts/Ads/AdRewardManager.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AdRewardManager : MonoBehaviour {
+    [SerializeField]
+    private int reward = 100;
+    [SerializeField]
+    private GameObject notification;
+    [SerializeField]
+    private GameObject showButton;
+
+    private PlayerController player;
+    private UnityAdsManager adManager;
+
+    void Start()
+    {
+        adManager = FindObjectOfType<UnityAdsManager>();
+        player = FindObjectOfType<PlayerController>();
+    }
+
+    public void OpenAdDialog()
+    {
+        notification.SetActive(true);
+        showButton.SetActive(false);
+    }
+
+    public void AcceptAds(string zoneID = "")
+    {
+        adManager.ShowAd(this, zoneID);
+    }
+
+    public void RewardPlayer(int typeOfReward)
+    {
+        switch (typeOfReward)
+        {
+            case 1:
+                player.dollarBalance += reward;
+                NotificationHandler.instance.MakeNotification("Thank You !", "You have earned " + reward + " coins!");
+                break;
+            case 2:
+                player.dollarBalance += (reward / 4);
+                NotificationHandler.instance.MakeNotification("Thank You !", "You have earned " + (reward/4) + " coins for watching part of the ad.");
+                break;
+            default:
+                Debug.Log("Reward switch error.");
+                break;
+        }
+    }
+
+    public void CancelAds()
+    {
+        notification.SetActive(false);
+        showButton.SetActive(true);
+    }
+}
---
+++ Assets/My Assets/_Scripts/Ads/UnityAdsManager.cs
@@ -0,0 +1,106 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Advertisements;
+
+public class UnityAdsManager : MonoBehaviour {
+
+    public bool testMode = true;
+    [SerializeField]
+    private string iOSGameID;
+    [SerializeField]
+    private string androidGameID;
+
+    private AdRewardManager rManager;
+
+    /*#if UNITY_EDITOR
+        void Start()
+        {
+            // Initialize Unity Ads here.
+            Advertisement.Initialize("69111", testMode);
+            DontDestroyOnLoad(this.gameObject);
+        }
+    #endif*/
+
+    #if UNITY_IOS
+	    void Start () 
+        {
+            // Initialize Unity Ads here.
+	        Advertisement.Initialize(iOSGameID, testMode);
+            DontDestroyOnLoad(this.gameObject);
+	    }
+    #endif
+
+    #if UNITY_ANDROID
+	    void Start () 
+        {
+            // Initialize Unity Ads here.
+	        Advertisement.Initialize(androidGameID, testMode);
+            DontDestroyOnLoad(this.gameObject);
+	    }
+    #endif
+
+    public void ShowAd(AdRewardManager rewardManager, string zoneID = "")
+    {
+        rManager = rewardManager;
+        #if UNITY_EDITOR
+            StartCoroutine(WaitForAd());
+        #endif
+
+        if (zoneID == "")
+        {
+            zoneID = null;
+        }
+
+        ShowOptions options = new ShowOptions();
+        options.resultCallback = AdCallBackHandler;
+
+        if (Advertisement.IsReady(zoneID))
+        {
+            Advertisement.Show(zoneID, options);
+        }
+        else if (Advertisement.IsReady())
+        {
+            Advertisement.Show(null, options);
+        }
+        else
+        {
+            Debug.Log("No ads are ready.");
+        }
+    }
+
+
+    private void AdCallBackHandler(ShowResult result)
+    {
+        switch (result)
+        {
+            case ShowResult.Finished:
+                //Debug.Log("finished");
+                rManager.RewardPlayer(1);
+                rManager.CancelAds();
+                break;
+            case ShowResult.Skipped:
+                //Debug.Log("Skipped");
+                rManager.RewardPlayer(2);
+                rManager.CancelAds();
+                break;
+            case ShowResult.Failed:
+                Debug.Log("The Show Result of the Ad is 'Failed'");
+                break;
+        }
+    }
+
+    private IEnumerator WaitForAd()
+    {
+        float currentTimeScale = Time.timeScale;
+        Time.timeScale = 0f;
+        yield return null;
+
+        while (Advertisement.isShowing)
+        {
+            yield return null;
+        }
+
+        Time.timeScale = currentTimeScale;
+    }
+
+}
